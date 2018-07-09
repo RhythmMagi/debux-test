3dbdd8924582a0196f6f765c0290099e9fe879be
Jeffrey Schrock <rhythmmagi@gmail.com>
2018-07-09 21:07:36 +0000

    added hamburger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,18 @@ const NavBar = (props) => {
   return (
     <div className = 'navBar'>
       <h1>De-Bux</h1>
+      <div class="options">
+        <div class="optionbtn">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="options-content">
+          <a onClick=''>Toggle Orientation</a>
+          <a onClick=''>Display Components Only</a>
+          <a onClick=''>Display Store Only</a>
+        </div>
+  	  </div>
     </div>
   );
 };
